8835f535c9380c4177f6be6e0561b5dbfb013e05
Sobhan-Ziyaei <sobhan.ziaee73@gmail.com>
2023-11-13 15:14:38 +0330

    create list of hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Hero } from '../models/hero';
+import { HEROES } from '../mock/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']
+  styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent {
-
+  heroes = HEROES;
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
